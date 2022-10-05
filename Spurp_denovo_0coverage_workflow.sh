7535a08cb5295729e36892b75145fa51c0ce079a
Gabriel Barrett <gabriel.barrett@uconn.edu>
2022-10-05 15:33:46 -0400

    adding 0 coverage workflow'

---
+++ Spurp_denovo_0coverage_workflow.sh
@@ -0,0 +1,72 @@
+#!/bin/local/env bash
+
+# ~~~~~~~~~~~~~~~
+# DE-NOVO CONSTRUCTION ON PROCESSED READW
+# ~~~~~~~~~~~~~~~
+
+cd /home/Shared_Data/Spurp_RAD/test
+
+# Cluster Reads
+bigfile1=../01-PROCESS/bigfile.1.fq.gz
+bigfile2=../01-PROCESS/bigfile.2.fq.gz
+rainbow cluster -1 $bigfile1 -2 $bigfile2 -m 4 -e 2000 > rbcluster.out 2> rbcluster.log
+
+rainbow div -i rbcluster.out -o rbdiv.out -k 7 -K 10 -f .5 2> rbdiv.log
+
+rainbow merge -i rbdiv.out -o rbmerge.out -a -r 2 -N1000 -R1000 -l 20 -f .9 2> rbmerge.log
+
+# Consensus De-Novo Reference
+/usr/local/bin/select_best_rbcontig_plus_read1.pl rbmerge.out rbdiv.out > rainbow.fasta
+
+# ~~~~~~~~~~~~~~~~~~
+# ALIGNING INDV TO DE-NOVO REFERENCE
+# ~~~~~~~~~~~~~~~~~~
+
+# Index De-Novo Reference Genome
+bwa index -a bwtsw rainbow.fasta -p rainbow
+
+FQ1=../01-PROCESS/*.out_1.fq.gz
+for fq1 in $FQ1;
+do
+	PREFIX=$(basename "$fq1" .out_1.fq.gz)
+	fq2=../01-PROCESS/"${PREFIX}.out_2.fq.gz"
+	BAM="${PREFIX}.bam"
+	RG=$(echo \@RG\\tID:$PREFIX\\tSM:$PREFIX)
+
+	# Align Reads
+	bwa mem -t 15 -R $RG rainbow $fq1 $fq2 | \
+	samtools view -S -h -q 10 - | \
+	samtools sort - > $BAM
+	samtools index $BAM
+
+	samtools coverage $BAM > $PREFIX.cov
+done
+
+OUT=FINAL_RESULTS
+mkdir ./${OUT}
+
+echo "read coverage meandepth meanbaseq meanmapq" > $OUT/avg_female.cov
+	# skip first line of female bam files
+
+	# stache coverage from each bam file and calculate average for each row
+awk '{
+	c[FNR]=$1;a[FNR]+=$6;d[FNR]+=$7;e[FNR]+=$8;f[FNR]+=$9;b[FNR]++;
+}END{
+	# skip the first line by setting i = 2
+	for(i=2;i<=FNR;i++)
+	print c[i],a[i]/b[i],d[i]/b[i],e[i]/b[i],f[i]/b[i]
+}' *f*.cov >> $OUT/avg_female.cov
+
+	# repeat for males
+echo "read coverage meandepth meanbaseq meanmapq" > $OUT/avg_male.cov
+awk '{
+        c[FNR]=$1;a[FNR]+=$6;d[FNR]+=$7;e[FNR]+=$8;f[FNR]+=$9;b[FNR]++;
+}END{
+        for(i=2;i<=FNR;i++)
+        print c[i],a[i]/b[i],d[i]/b[i],e[i]/b[i],f[i]/b[i]
+}' *m*.cov >> $OUT/avg_male.cov
+
+# Write reads with 0 coverage to FINAL_RESULTS respective of sex
+
+awk '{if ($3 == 0) print}' $OUT/avg_male.cov > $OUT/avg_male_0.cov
+awk '{if ($3 == 0) print}' $OUT/avg_female.cov > $OUT/avg_female_0.cov
